946571a9e01f4e5f0e774742b7b58f55d132144f
AyushmaanAggarwal <ayu02468@gmail.com>
2024-10-13 15:34:31 -0700

    Fix stop autocompile for vimwiki

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,5 +1,13 @@
 local autocmd_group = vim.api.nvim_create_augroup('Custom auto-commands', { clear = true })
 
+InFolder = function(folder)
+    local cwDir = vim.fn.getcwd()
+    if string.match(cwDir, folder) then
+      return true
+    end
+    return false
+end
+
 vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   pattern = { '*.py' },
   desc = 'Auto-format Python files after saving',
@@ -16,12 +24,14 @@ vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
   pattern = { '*.md' },
   desc = 'Auto-compile Markdown files after saving',
   callback = function()
-    local raw_file_name = vim.api.nvim_buf_get_name(0)
-    local file_name = raw_file_name:gsub('%s', '\\ ')
-    local raw_file_pdf = string.sub(raw_file_name, 1, -3) .. "pdf"
-    local file_pdf = raw_file_pdf:gsub('%s', '\\ ')
-    vim.print(file_pdf)
-    vim.cmd(string.format(":!pandoc -s %s -o %s --pdf-engine=pdflatex", file_name, file_pdf))
+    if not InFolder("Obsidian") then
+      local raw_file_name = vim.api.nvim_buf_get_name(0)
+      local file_name = raw_file_name:gsub('%s', '\\ ')
+      local raw_file_pdf = string.sub(raw_file_name, 1, -3) .. "pdf"
+      local file_pdf = raw_file_pdf:gsub('%s', '\\ ')
+      vim.print(file_pdf)
+      -- vim.cmd(string.format(":!pandoc -s %s -o %s --pdf-engine=pdflatex", file_name, file_pdf))
+    end
   end,
   group = autocmd_group,
 })
