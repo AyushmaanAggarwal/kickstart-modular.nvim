6e380ae1771619c133e34f783dd15fe263152885
AyushmaanAggarwal <ayu02468@gmail.com>
2025-03-27 20:49:58 -0700

    Add a few cs related snippets

--- lua/custom/snippets/markdown/main.lua
+++ lua/custom/snippets/markdown/main.lua
@@ -1,30 +1,29 @@
 local ls = require("luasnip")
-local s = ls.snippet
-local sn = ls.snippet_node
-local isn = ls.indent_snippet_node
-local t = ls.text_node
+local s = ls.snippet -- local sn = ls.snippet_node
+-- local isn = ls.indent_snippet_node
+-- local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
-local c = ls.choice_node
-local d = ls.dynamic_node
-local r = ls.restore_node
-local events = require("luasnip.util.events")
-local ai = require("luasnip.nodes.absolute_indexer")
-local extras = require("luasnip.extras")
-local l = extras.lambda
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+-- local extras = require("luasnip.extras")
+-- local l = extras.lambda
 local rep = extras.rep
-local p = extras.partial
-local m = extras.match
-local n = extras.nonempty
-local dl = extras.dynamic_lambda
+-- local p = extras.partial
+-- local m = extras.match
+-- local n = extras.nonempty
+-- local dl = extras.dynamic_lambda
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
-local conds = require("luasnip.extras.expand_conditions")
-local postfix = require("luasnip.extras.postfix").postfix
-local types = require("luasnip.util.types")
-local parse = require("luasnip.util.parser").parse_snippet
-local ms = ls.multi_snippet
-local k = require("luasnip.nodes.key_indexer").new_key
+-- local conds = require("luasnip.extras.expand_conditions")
+-- local postfix = require("luasnip.extras.postfix").postfix
+-- local types = require("luasnip.util.types")
+-- local parse = require("luasnip.util.parser").parse_snippet
+-- local ms = ls.multi_snippet
+-- local k = require("luasnip.nodes.key_indexer").new_key
 local date = function() return {os.date('%m/%d/%Y')} end
 
 return {
@@ -55,9 +54,9 @@ return {
     
     ]], {i(1), f(date, {})})),
 
-  s({ trig = "cs", name = "Metadata for Math HW" }, fmta([[
+  s({ trig = "cs", name = "Metadata for CS HW" }, fmta([[
     ---
-    title: CS170 - Homework <>
+    title: CS 170 - Homework <>
     author: Ayushmaan Aggarwal
     date: <>
     header-includes: |
@@ -74,26 +73,16 @@ return {
       \newcommand{\norm}{\trianglelefteq}
       \newcommand{\lt}{\ensuremath <<}
       \newcommand{\gt}{\ensuremath >>}
-
       \renewcommand{\P}{\mathbb P}
       \renewcommand{\l}{\langle}
       \renewcommand{\r}{\rangle}
       \renewcommand{\Re}{\operatorname{Re}}
       \renewcommand{\Im}{\operatorname{Im}}
-  
-      \usepackage{float}
-      \let\origfigure\figure
-      \let\endorigfigure\endfigure
-      \renewenvironment{figure}[1][2] {
-          \expandafter\origfigure\expandafter[H]
-      } {
-          \endorigfigure
-      }
     ---
-    
     \tableofcontents
     \newpage 
-
+    
+    # Problem 1
     ]], {i(1), f(date, {})})),
 
 
@@ -109,12 +98,6 @@ return {
     ]], {i(1), i(2)})),
 
 s({ trig = "algo", name = "Problem for CS Algorithm" }, fmt([[
-  # Problem {}
-  > {}
-
-  **Solution**
-
-
   **Algorithm description**
   <!-- This can come in terms of pseudocode, or a description in English. It must be unambiguous, as short as possible (but no shorter), and precise. Your pseudocode does not need to be executable. You should use notation such as “add X to set S” or “for each edge in graph G”. Remember you are writing your pseudocode to be read by a human, not a computer. See DPV for examples of pseudocode.-->
 
@@ -132,10 +115,20 @@ s({ trig = "algo", name = "Problem for CS Algorithm" }, fmt([[
 
   \newpage
 
-    ]], {i(1), i(2), i(3), i(4), i(5)})),
+    ]], {i(1), i(2), i(3)})),
+
+s({ trig = "code", name = "psuedocode block" }, fmt([[
+    ```psuedocode
+    {}
+    ```
+    ]], {i(1)})),
 
   s({ trig = "\\\\", name = "Comment" }, fmt([[<!-- {} -->]],
     {i(1)})),
+
+  s({ trig = "part", name = "Part" }, fmt([[### Part {}]],
+    {i(1)})),
+
 
   s({ trig = "fig", name = "Insert Figure" }, fmta([[![<>](./<>){width=300px}]],
     {i(1), i(2)})),
